ee746a90abf6263ca9b5ce9423c78eaee7e4507d
martinsibuchim <martinsibuchim@gmail.com>
2023-08-17 20:09:51 +0100

    list tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,5 @@
 -- List tables in the database
-SELECT table_name AS Tables_in_mysql
+SELECT table_name AS Tables_in_database
 FROM information_schema.tables
-WHERE table_schema = DATABASE(mysql);
+WHERE table_schema = DATABASE();
+
