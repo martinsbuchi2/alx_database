4932ee342113c975b4e26644a8a5af09e01b1b1e
martinsibuchim <martinsibuchim@gmail.com>
2023-08-18 00:55:46 +0100

    description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- full description of your table
-SELECT table_name, table_type
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE `first_table`;
