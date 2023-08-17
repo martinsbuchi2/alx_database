cca88afe11dfc632b671abf82f995b7d4b2cc599
martinsibuchim <martinsibuchim@gmail.com>
2023-08-17 22:39:02 +0100

    description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,4 @@
--- the full description of the table
-
-SELECT 
-    'Table   Create Table' AS header,
-    CONCAT(table_name, ' ', column_create_statement) AS create_statement
-FROM (
-    SELECT 
-        table_name,
-        GROUP_CONCAT(column_name, ' ', column_type) AS column_create_statement
-    FROM information_schema.columns
-    WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table'
-    GROUP BY table_name
-) AS column_info;
+-- full description of your table
+SELECT table_name, table_type
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
