007c71a18951d1fcd266f032f75ea069d728cf9d
martinsibuchim <martinsibuchim@gmail.com>
2023-08-22 16:43:32 +0100

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,13 @@
+-- Creates the database
+CREATE DATABASE 
+    IF NOT EXISTS `hbtn_0d_2`;
+
 -- script that creates the database and the user 
 CREATE USER 
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 
--- Creates the database
-CREATE DATABASE 
-    IF NOT EXISTS `hbtn_0d_2`;
-
 -- Grant permissions
 GRANT SELECT
-    ON `hbtn_0d_2` .*
+    ON `hbtn_0d_2`.*
     TO 'user_0d_2'@'localhost';
