9d00a2d5855c0f56ba697e31cdbd33d31597bf33
martinsibuchim <martinsibuchim@gmail.com>
2023-08-22 13:47:16 +0100

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,9 @@
--- Check if the user user_0d_1 already exists
-SELECT 1 FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost' LIMIT 1;
+-- script that creates the MySQL server user
+CREATE USER 
+IF NOT EXISTS user_0d_1
+IDENTIFIED BY 'user_0d_1_pwd';
 
--- Create the user only if it doesn't exist
-INSERT IGNORE INTO mysql.user (User, Host, Password) VALUES ('user_0d_1', 'localhost', PASSWORD('user_0d_1_pwd'));
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+-- Grant permissions
+GRANT ALL PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost';
