4d49f3e57205ff359474e42c9dc909a4f5ac2604
martinsibuchim <martinsibuchim@gmail.com>
2023-08-22 12:57:25 +0100

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,5 @@
--- script that lists all privileges of the MySQL users
--- Create user user_0d_1
-CREATE USER 'user_0d_1'@'localhost';
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2
-CREATE USER 'user_0d_2'@'localhost';
-
--- Grant necessary privileges to user_0d_2
--- Replace 'database_name' with the actual name of the database
-GRANT SELECT, INSERT, UPDATE ON database_name.* TO 'user_0d_2'@'localhost';
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
