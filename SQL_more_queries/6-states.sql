00b7c19b22ec11cc06f4d15918248748fa212e32
martinsibuchim <martinsibuchim@gmail.com>
2023-08-23 17:43:28 +0100

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- script that creates the database
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_usa`;
+
+CREATE TABLE
+    IF NOT EXISTS `states` (
+        id INT NOT NULL DEFAULT 1 UNIQUE AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(256) NOT NULL
+    );
