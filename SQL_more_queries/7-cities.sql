e7df940710771c0a43e95a0b577fe43ccef02a34
martinsibuchim <martinsibuchim@gmail.com>
2023-08-23 18:06:38 +0100

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,12 +1,10 @@
--- script that creates the database
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_usa`;
-
-CREATE TABLE
-    IF NOT EXISTS `cities` (
-        id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
-        state_id INT NOT NULL,
-        name VARCHAR(256) NOT NULL,
-        FOREIGN KEY (`state_id`),
-        REFERENCES `hbtn_0d_usa`.`states`(`id`)
-    );
+-- Creates the database hbtn_0d_usa with the table cities.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
+);
